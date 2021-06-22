9aab5e07f430896044cdb75f82cc03157ef05364
rizkydipahiyat <60695993+rizkydipahiyat@users.noreply.github.com>
2021-06-22 21:02:51 +0700

    Single  Product & Resource Route

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/products', [ProductController::class, 'store']);
+
+Route::resource('products', ProductController::class);
+
+// Route::get('/products', [ProductController::class, 'index']);
+// Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
